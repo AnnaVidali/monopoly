60b9813aff47d411fc3a62129921f7b2e7d629f1
Fotis <Fotis@PS-PC2>
2020-11-24 21:50:00 +0200

    throw dice method

--- Monopoly/src/methods/PlayerMethods.java
+++ Monopoly/src/methods/PlayerMethods.java
@@ -3,12 +3,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package methods;
 
+package methods;
+import information.*;
+import java.util.Random;
 /**
  *
  * @author anna
  */
 public class PlayerMethods {
+    
+    
+  int ThrowDice() {
+      int numSides=6;
+       Random  r = new Random();
+      int number = r.nextInt(numSides)+1;
+     return number;
+}  
+    
+    
+    
+    
+    
     
 }
